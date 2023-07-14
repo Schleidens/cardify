1ba58478d03e1615e4e82d599ee2451a7253709a
Schleidens <alectineschleidens2016@gmail.com>
2023-07-14 13:05:21 -0400

    fix broken link in edit this page githb redirect setting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,7 +42,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/schleidens/cardify/",
+          editUrl: "https://github.com/schleidens/cardify/edit/master/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
